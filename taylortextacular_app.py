de1ec5a71abc34d062636fb622cb1b4f3d8f576e
Michelle Wang <michellewang0402@gmail.com>
2023-02-17 17:44:10 -0500

    made visualization resusable and made small edits

--- taylortextacular_app.py
+++ taylortextacular_app.py
@@ -40,7 +40,7 @@ def wordcount_sankey(data, word_list=None, k=5):
 
     word_count = list(zip(all_words, all_counts, texts))
     df_word_counts = pd.DataFrame(word_count, columns=['Word', 'Counts', 'Text'])
-    print(df_word_counts)
+    # print(df_word_counts)
     sk.make_sankey(df_word_counts, 0, 'Text', 'Word', vals=df_word_counts['Counts'])
 
 
@@ -51,14 +51,14 @@ def main():
     # register some text files
     ts.load_text('TaylorSwiftOurSong.txt', 'Our Song')
     ts.load_text('TaylorSwiftFearless.txt', 'Fearless')
-    ts.load_text('TaylorSwiftCardigan.txt', 'Cardigan')
-    ts.load_text('TaylorSwiftDearJohn.txt', 'Dear John')
-    ts.load_text('TaylorSwiftGetawayCar.txt', 'Getaway Car')
-    ts.load_text('TaylorSwiftLavenderHaze.txt', 'Lavender Haze')
-    ts.load_text('TaylorSwiftLover.txt', 'Lover')
-    ts.load_text('TaylorSwiftRed.txt', 'Red')
-    ts.load_text('TaylorSwiftWelcometoNewYork.txt', 'Welcome to New York')
-    ts.load_text('TaylorSwiftWillow.txt', 'Willow')
+    # ts.load_text('TaylorSwiftCardigan.txt', 'Cardigan')
+    # ts.load_text('TaylorSwiftDearJohn.txt', 'Dear John')
+    # ts.load_text('TaylorSwiftGetawayCar.txt', 'Getaway Car')
+    # ts.load_text('TaylorSwiftLavenderHaze.txt', 'Lavender Haze')
+    # ts.load_text('TaylorSwiftLover.txt', 'Lover')
+    # ts.load_text('TaylorSwiftRed.txt', 'Red')
+    # ts.load_text('TaylorSwiftWelcometoNewYork.txt', 'Welcome to New York')
+    # ts.load_text('TaylorSwiftWillow.txt', 'Willow')
 
     # product sankey diagram with all 10 files
     ts.load_visualization('sankey1', wordcount_sankey)
